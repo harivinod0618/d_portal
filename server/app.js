9134696460a2014157defe031fad70d5fb772ed9
harivinod0618 <harivinod1018@gmail.com>
2024-11-05 11:59:54 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -28,7 +28,7 @@ connectDB();
 
 //
 app.get("/",(req,res)=>{
-  res.json("welcome to convocation")
+  res.json("Hari Vinod's maneya....!!")
 })
 //routes
 const userRoutes = require("./routes/User");
